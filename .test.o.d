42d4ba028d13f8fd284fdbba62610def9d787d1b
ivbakula <84186651+ivbakula@users.noreply.github.com>
2021-05-14 18:46:23 +0200

    Delete .test.o.d

--- .test.o.d
+++
@@ -1,5 +0,0 @@
-test.o: /home/ivan/Documents/programiranje/linux/ldd/char/test.c \
- include/linux/kconfig.h include/generated/autoconf.h \
- include/linux/compiler_types.h include/linux/compiler_attributes.h \
- include/linux/compiler-gcc.h include/linux/kdev_t.h \
- include/uapi/linux/kdev_t.h
